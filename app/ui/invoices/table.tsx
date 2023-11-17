c8d16046f75878cec418a452dfaae6c3f4863196
Timothy Lim <timothyytl@gmail.com>
2023-11-17 13:13:36 +0800

    updated table.tsx

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -31,7 +31,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt="invoice-img"
+                        alt="invoice"
                       />
                       <p>{invoice.name}</p>
                     </div>
